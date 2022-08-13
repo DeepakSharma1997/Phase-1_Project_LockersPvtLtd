9eea35e7ce2750459b86bb2cdb2d9c083f7b2235
Deepak Sharma <Deepak.rockstar29@gmail.com>
2022-08-13 23:09:20 +0530

    This is my first commit

---
+++ src/com/project/lockedme/Main.java
@@ -0,0 +1,14 @@
+package com.project.lockedme;
+
+public class Main {
+	
+	/*Enter your desired Directory path */
+	public static final String path = "D:\\Final FSJD Data\\FSJD Ecllipse\\LockersPvtLtd\\src\\com\\project\\lockedme"; 
+	
+	public static void main(String[] args) {
+		Menus menu = new Menus();
+		menu.introScreen();
+		menu.mainMenu();
+	}
+
+}
